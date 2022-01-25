86df0b33ae4366466f9e96c261ce5392f7ad36ff
Alexandre Fernandez <a.fernandez.martinez@outlook.com>
2022-01-25 16:13:06 +0100

    v1.1.1

--- src/Database.php
+++ src/Database.php
@@ -4,15 +4,20 @@ namespace App;
 use \PDO;
 
 class Database {
+	private static $pdo;
+
 	public static function getPdo() {
-		return new PDO(
-			"mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", 
-			$_ENV["DB_USER"], 
-			$_ENV["DB_PASSWORD"], 
-			[
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-			]
-		);
+		if(!self::$pdo) {
+			self::$pdo = new PDO(
+				"mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", 
+				$_ENV["DB_USER"], 
+				$_ENV["DB_PASSWORD"], 
+				[
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+				]
+			);
+		}
+		return self::$pdo;
 	}
 }
